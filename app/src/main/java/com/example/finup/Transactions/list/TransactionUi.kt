c4cfcdf6250d61a7288ad67079d954bd969dd0d9
aman <kemalov2004@mail.ru>
2025-09-24 20:52:36 +0500

    add TransactionUi

---
+++ app/src/main/java/com/example/finup/Transactions/list/TransactionUi.kt
@@ -0,0 +1,11 @@
+package com.example.finup.Transactions.list
+
+data class TransactionUi(
+    val id: Long,
+    val name: String,
+    val sum: Int,
+    val type: String,
+    val day: Int,
+    val dateId: Long,
+
+)
