9249c2f2de828569c09f501a085a684c75b0ebc5
aman <kemalov2004@mail.ru>
2025-10-01 12:58:36 +0500

    add CleanUpEmptyPeriodUseCaseTest and use case

--- app/src/main/java/com/example/finup/Transactions/createEdit/UseCases/CleanUpEmptyPeriodUseCase.kt
+++ app/src/main/java/com/example/finup/Transactions/createEdit/UseCases/CleanUpEmptyPeriodUseCase.kt
@@ -1,5 +1,23 @@
 package com.example.finup.Transactions.createEdit.UseCases
 
+import com.example.finup.domain.TransactionRepository
+import com.example.finup.domain.YearMonthRepository
+
 interface CleanUpEmptyPeriodUseCase {
+
     suspend operator fun invoke(dateId: Long)
+
+    class Base(
+        private val transactionRepository: TransactionRepository.GetTransactions,
+        private val yearMonthRepository: YearMonthRepository.Delete
+    ) : CleanUpEmptyPeriodUseCase {
+
+        override suspend fun invoke(dateId: Long) {
+            val currentExpenseTransactionList = transactionRepository.getTransactions(dateId,"Expense")
+            val currentIncomeTransactionList = transactionRepository.getTransactions(dateId,"Income")
+            if(currentExpenseTransactionList.isEmpty() && currentIncomeTransactionList.isEmpty()) {
+                yearMonthRepository.delete(dateId)
+            }
+        }
+    }
 }
