77bb306f600690dfb71df735c3329bd8b1584a17
aman <kemalov2004@mail.ru>
2025-10-01 14:09:59 +0500

    add NavigationMonthUseCaseTest

--- app/src/test/java/com/example/finup/Transactions/list/TransactionListUseCasesTest/NavigationMonthUseCaseTest.kt
+++ app/src/test/java/com/example/finup/Transactions/list/TransactionListUseCasesTest/NavigationMonthUseCaseTest.kt
@@ -1,6 +1,9 @@
 package com.example.finup.Transactions.list.TransactionListUseCasesTest
 
 import com.example.finup.Transactions.list.useCases.NavigationMonthUseCase
+import com.example.finup.domain.YearMonth
+import com.example.finup.domain.YearMonthRepository
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
@@ -12,6 +15,40 @@ class NavigationMonthUseCaseTest {
     @Test
     fun test() = runBlocking {
         yearMonthRepository = FakeYearMonthRepository.Base()
-    }
+        navigationMonthUseCase = NavigationMonthUseCase.Base(yearMonthRepository)
+        yearMonthRepository.expectedListYearMonth(listOf(YearMonth(1L, 11, 2025), YearMonth(2L,12,2025)))
+        val actualYearMonth = navigationMonthUseCase(YearMonth(2L, 12, 2025), false)
+        val expectedYearMonth = YearMonth(1L,11,2025)
+        assertEquals(expectedYearMonth,actualYearMonth)
 
+
+        val actual = navigationMonthUseCase(YearMonth(1L,11,2025),false)
+        val expected = YearMonth(1L,11,2025)
+        assertEquals(actual,expected)
+    }
+}
+
+private interface FakeYearMonthRepository : YearMonthRepository.GetAllPeriods {
+
+    fun expectedListYearMonth(expected: List<YearMonth>)
+    fun checkCalledTimes(expected: Int)
+
+    class Base : FakeYearMonthRepository {
+
+        private lateinit var mockedList: List<YearMonth>
+        private var actualCalledTime = 0
+
+        override suspend fun getAllPeriods(): List<YearMonth> {
+            actualCalledTime++
+            return mockedList
+        }
+
+        override fun expectedListYearMonth(expected: List<YearMonth>) {
+            mockedList = expected
+        }
+
+        override fun checkCalledTimes(expected: Int) {
+            assertEquals(actualCalledTime, expected)
+        }
+    }
 }
